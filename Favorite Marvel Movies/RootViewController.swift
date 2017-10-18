a903ee4559533fcd8cbd8b037646e759d8aaef90
lauriezip <lauriezip@gmail.com>
2017-10-18 10:12:12 -0400

    Initial Commit

---
+++ Favorite Marvel Movies/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  Favorite Marvel Movies
+//
+//  Created by Laurie Zipperer on 10/18/17.
+//  Copyright © 2017 Codebase. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
